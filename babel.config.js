a652cdf32b5cd158339277f412db94b4b6926208
zhanglijie5997 <13650653625@163.com>
2020-09-27 09:10:34 +0800

    🔧  jsx插件修改

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   "plugins": [
-    ["@hcysunyang/vue-next-jsx", {
+    ["@vue/babel-plugin-jsx", {
       // Specify source
       "source": "@vue/runtime-dom"
     }],
